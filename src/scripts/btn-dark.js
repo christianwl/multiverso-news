ad160f8576011fd83f0c5f232147e2cbbe162cfc
christianwl <chris2w0social@gmail.com>
2026-02-28 02:02:35 -0300

    styles: update styles and add sun/moon icon

--- src/scripts/btn-dark.js
+++ src/scripts/btn-dark.js
@@ -1,5 +1,12 @@
 const btnDark = document.getElementById("btn-dark");
+const icon = btnDark.querySelector("i")
+
+document.addEventListener('DOMContentLoaded', () => {
+  icon.classList.toggle("fa-sun");
+})
 
 btnDark.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  icon.classList.toggle("fa-sun");
+  icon.classList.toggle("fa-moon");
 });
